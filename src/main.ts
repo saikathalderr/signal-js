8a784f8734a31a85f259b38bbcb6699e6315db69
Saikat Halder <saikat.halder@lotto24.de>
2025-02-25 09:21:05 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,38 @@
 import './style.css'
+import {createEffect, createSignal} from "./signal/signal.ts";
+import {Pokemon} from "./pokemon/types.ts";
+import {fetchAllPokemons} from "./pokemon/pokemon.ts";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1 class="text-3xl font-bold underline">
-        Let's start coding!
-    </h1>
-  </div>
-`
+const list = document.querySelector<HTMLDivElement>('#list')!
+const info = document.querySelector<HTMLDivElement>('#info')!
+
+const [pokemons, setPokemons] = createSignal<Pokemon[]>([])
+const [selectedPokemon, setSelectedPokemon] = createSignal<Pokemon | null>(null)
+
+createEffect(() => {
+    list.innerHTML = pokemons().map(pokemon => `
+        <div class="flex items-center"
+                onclick="() => setSelectedPokemon(pokemon)"
+        >
+            <img class="w-10" src="${pokemon.sprites.front_default}" alt="${pokemon.name}" />
+            <div>${pokemon.name}</div>
+        </div>
+`).join('')
+})
+
+createEffect(() => {
+    if (selectedPokemon()) {
+        info.innerHTML = `
+            <h2>${selectedPokemon()?.name}</h2>
+            <img src="${selectedPokemon()?.sprites.front_default}" alt="${selectedPokemon()?.name}" />
+            <div>Height: ${selectedPokemon()?.height}</div>
+            <div>Weight: ${selectedPokemon()?.weight}</div>
+        `
+    } else {
+        info.innerHTML = ''
+    }
+})
+
+
+fetchAllPokemons().then(setPokemons)
 
---
+++ src/pokemon/pokemon.ts
@@ -0,0 +1,28 @@
+import {BasePokemon, Pokemon} from "./types.ts";
+
+const POKEMONS_API_URL = 'https://pokeapi.co/api/v2/pokemon?limit=10'
+
+interface FetchBasePokemonsResponse {
+    results: BasePokemon[]
+    count: number
+    previous?: string
+    next?: string
+}
+
+async function fetchBasePokemons(): Promise<FetchBasePokemonsResponse> {
+    const resp = await fetch(POKEMONS_API_URL)
+    return await resp.json()
+}
+
+async function fetchPokemon(url: string): Promise<Pokemon> {
+    const resp = await fetch(url)
+    return await resp.json()
+}
+
+async function fetchAllPokemons(): Promise<Pokemon[]> {
+    const { results: pokemons } = await fetchBasePokemons()
+    const promises = pokemons.map(pokemon => fetchPokemon(pokemon.url))
+    return await Promise.all(promises)
+}
+
+export { fetchAllPokemons }
